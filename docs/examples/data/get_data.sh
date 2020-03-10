dc121c6808d20fd5902c6292d43348b17925112a
Phelelani Mpangase <pmpangase@gmail.com>
2020-03-10 18:39:54 +0200

    Script for getting the SRP094510 sequences!

---
+++ docs/examples/data/get_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ftp_files=(ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR507/001/SRR5074531/SRR5074531_1.fastq.gz
+    ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR507/001/SRR5074531/SRR5074531_2.fastq.gz 
+    ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR507/000/SRR5074530/SRR5074530_1.fastq.gz
+    ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR507/000/SRR5074530/SRR5074530_2.fastq.gz
+    ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR507/009/SRR5074529/SRR5074529_1.fastq.gz
+    ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR507/009/SRR5074529/SRR5074529_2.fastq.gz
+    ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR507/008/SRR5074528/SRR5074528_1.fastq.gz
+    ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR507/008/SRR5074528/SRR5074528_2.fastq.gz)
+
+for url in "${links[@]}"
+do
+    echo ${url}
+    lftp -e 'pget -n 5 -c '"${url}"'; bye'
+done
